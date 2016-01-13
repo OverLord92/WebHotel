f3b6577ce47441f9dc35d42cddccfd402d37a016
GEDORA <GEDORA@GEDORA-PC>
2016-01-13 17:48:33 +0100

    archived users zamjenjen sa enabled kolonom

--- src/com/hotel/dao/User.java
+++ src/com/hotel/dao/User.java
@@ -37,107 +37,98 @@ public class User {
 	private Collection<Service> services = new ArrayList<>();
 	
 	private Date checkInDate;
-	
 	private boolean online; // pomocu ovog polja ce admin provjeravati da li je user online
-
+	
+	@Column(columnDefinition="TINYINT(1)")
+	private boolean enabled;
+	private String authority;
+	
+	
 	
 	
 	public String getUsername() {
 		return username;
 	}
-
 	public void setUsername(String username) {
 		this.username = username;
 	}
-
 	public String getPassword() {
 		return password;
 	}
-
 	public void setPassword(String password) {
 		this.password = password;
 	}
-
 	public String getEncodedPassword() {
 		return encodedPassword;
 	}
-
 	public void setEncodedPassword(String encodedPassword) {
 		this.encodedPassword = encodedPassword;
 	}
-
 	public String getName() {
 		return name;
 	}
-
 	public void setName(String name) {
 		this.name = name;
 	}
-
 	public String getLastName() {
 		return lastName;
 	}
-
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
-
 	public String getIdNumber() {
 		return idNumber;
 	}
-
 	public void setIdNumber(String idNumber) {
 		this.idNumber = idNumber;
 	}
-
 	public Date getDob() {
 		return dob;
 	}
-
 	public void setDob(Date dob) {
 		this.dob = dob;
 	}
-
 	public String getGender() {
 		return gender;
 	}
-
 	public void setGender(String gender) {
 		this.gender = gender;
 	}
-
 	public Room getRoom() {
 		return room;
 	}
-
 	public void setRoom(Room room) {
 		this.room = room;
 	}
-	
 	public Collection<Service> getServices() {
 		return services;
 	}
-
 	public void setServices(Collection<Service> services) {
 		this.services = services;
 	}
-
 	public Date getCheckInDate() {
 		return checkInDate;
 	}
-
 	public void setCheckInDate(Date checkInDate) {
 		this.checkInDate = checkInDate;
 	}
-
 	public boolean isOnline() {
 		return online;
 	}
-
 	public void setOnline(boolean online) {
 		this.online = online;
 	}
-	
-	 
+	public boolean isEnabled() {
+		return enabled;
+	}
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+	public String getAuthority() {
+		return authority;
+	}
+	public void setAuthority(String authority) {
+		this.authority = authority;
+	}
 
 }
