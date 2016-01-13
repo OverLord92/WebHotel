7da82135b7662effee61be7c6ac6da7601f79a43
GEDORA <GEDORA@GEDORA-PC>
2016-01-13 20:08:10 +0100

    dodana value kolona u user_request

--- src/com/hotel/dao/UserRequest.java
+++ src/com/hotel/dao/UserRequest.java
@@ -6,15 +6,23 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToOne;
+import javax.persistence.Table;
 
 @Entity
+@Table(name="user_requests")
 public class UserRequest {
 	
 	@Id
 	@GeneratedValue(strategy=GenerationType.TABLE)
 	private int id;
 	
+	//tip requesta moze biti changeRoomType, reserveService i signOut
 	private String type;
+	
+	// ukoliko je request changeRoomType value moze biti novi tip sobe
+	// ako je request reserveService onda je value naziv servisa koji
+	// korisnik zeli rezervisati
+	private String value; 
 	
 	@OneToOne(cascade=CascadeType.ALL)
 	private User user;
@@ -36,6 +44,14 @@ public class UserRequest {
 
 	public void setType(String type) {
 		this.type = type;
+	}
+	
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
 	}
 
 	public User getUser() {
